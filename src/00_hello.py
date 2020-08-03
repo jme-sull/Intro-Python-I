6c35f5b2221be22d6ac35b44e5e9c286ac6a4804
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-08-03 15:03:03 -0400

    about half way complete - monday mvp

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,12 @@
 # Print "Hello, world!" to your terminal
+
+print("Hello, world")
+
+name = "Sean"
+
+print(f"Hello, {name}")
+
+if name != "Jamie":
+    print("Hey, you aren't Jamie")
+else:
+    print(f"Hello, {name}")
--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,14 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def even(n):
+    if (n % 2) == 0:
+        return True
+    else:
+        return False
+
+num = 2
+print(even(num))
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -9,4 +17,11 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def evenPrint(n):
+    if (n % 2) == 0:
+        return print("Even!")
+    else:
+        return print("Odd!")
+
+print(evenPrint(num))
 
